7f0bfce27551657841ae3f3179888311bdd4003a
Santiago-Duque <sduque@ucsd.edu>
2025-03-03 13:22:44 -0800

    Finished Mask/Char Feature Part 3

--- script.js
+++ script.js
@@ -1,49 +1,100 @@
-/*/const inputname = document.querySelector("#name");
-const inputemail = document.querySelector("#email");
-const inputmsg = document.querySelector("#msg");
-const checkbtn = document.querySelector("#check");
-const output = document.querySelector("#log");
+document.addEventListener("DOMContentLoaded", (e) => {
+    const inputname = document.getElementById("name");
+    const inputemail = document.getElementById("email");
+    const inputmsg = document.getElementById("msg");
+    const errorname = document.getElementById("error-name");
+    const erroremail = document.getElementById("error-email");
+    const errormsg = document.getElementById("error-msg");
+    const infomsg = document.getElementById("info-message");
+    const maxlength = 100;
 
-inputname.addEventListener("invalid", () => {
-    console.log("Invalid Name Entered");
-});
+    inputname.addEventListener("input", (e) => {
 
-inputemail.addEventListener("invalid", () => {
-    console.log("Invalid Email Address Entered");
-});
-
-inputmsg.addEventListener("invalid", () => {
-    console.log("Invalid Message");
-});
-
-
-checkbtn.addEventListener("click", () => {
-    const checkname = inputname.checkValidity();
-    const checkemail = inputemail.checkValidity();
-    const checkmsg = inputmsg.checkValidity();
-    output.innerHTML = `checkValidity returned: ${checkname}`;
-    output.innerHTML = `checkValidity returned: ${checkemail}`;
-    output.innerHTML = `checkValidity returned: ${checkmsg}`;
-});*/
-
-/*document.querySelectorAll("input, textarea").forEach((inputfield) => {
-    inputfield.addEventListener("keypress", (e) => {
-        const validChars = /^[a-zA-z0-9 @.,!?]*$/;
-        if (!validChars.test(e.key)) {
-            e.preventDefault();
+        if (inputname.value.length == 0) {
+            errormsg.textContent = "";
+            inputname.style.border = "1px solid #1e459c";
         }
 
-        const errormsg = inputfield.nextElementSibling;
-        if (errormsg && (errormsg.classList.contains("error-name") || errormsg.classList.contains("error-email") || errormsg.classList.contains("error-msg"))) {
-            errormsg.style.display = "inline";
-            errormsg.textContent = "Invalid Character Detected";
-            errormsg.style.color = "red";
+        if (inputname.validity.valid == true) {
+            errorname.textContent = "";
+            inputname.style.border = "3px solid rgb(100, 253, 100)";
         }
-
-        inputfield.style.border = "2px solid red";
-        setTimeout(() => {
-            inputfield.style.border = "";
-            inputfield.nextElementSibling.style.display = "none";
-        }, 5000);
+        
+        else if (inputname.validity.patternMismatch == true) {
+            errorname.textContent = "Sorry, That Character Isn't Allowed";
+            inputname.value = inputname.value.slice(0, -1);
+            errorname.style.color = "red";
+            inputname.style.border = "10px solid red";
+            setTimeout(() => {
+                errorname.textContent = "";
+                inputname.style.border = "1px solid #1e459c";
+            }, 2000);
+        }
     });
-});*/
+    
+    /*inputemail.addEventListener("input", (e) => {
+
+        if (inputemail.validity.valid == true) {
+            erroremail.textContent = "";
+            inputemail.style.border = "3px solid rgb(100, 253, 100)";
+        }
+        
+        else if (inputemail.validity.typeMismatch == true) {
+            erroremail.textContent = "Sorry, That Character Isn't Allowed";
+            erroremail.style.color = "red";
+            inputemail.style.border = "10px solid red";
+            setTimeout(() => {
+                erroremail.textContent = "";
+                inputemail.style.border = "1px solid #1e459c";
+            }, 2000);
+        }
+    });*/
+
+    inputmsg.addEventListener("input", (e) => {
+
+         validPattern = /^[a-zA-Z0-9\s.,!?']+$/;
+
+        if (validPattern.test(inputmsg.value)) {
+            errormsg.textContent = "";
+            inputmsg.style.border = "3px solid rgb(100, 253, 100)";
+        }
+        
+        else {
+            errormsg.textContent = "Sorry, That Character Isn't Allowed";
+            inputmsg.value = inputmsg.value.slice(0, -1);
+            errormsg.style.color = "red";
+            inputmsg.style.border = "10px solid red";
+            setTimeout(() => {
+                errormsg.textContent = "";
+                inputmsg.style.border = "1px solid #1e459c";
+            }, 2000);
+        }
+    });
+
+    inputmsg.addEventListener("input", (e) => {
+
+        let remainingchars = maxlength - inputmsg.value.length;
+
+        infomsg.textContent = `${remainingchars} chars remaining`;
+
+        if (inputmsg.value.length == 0) {
+            errormsg.textContent = "";
+            infomsg.textContent = "";
+            inputmsg.style.border = "1px solid #1e459c";
+        }
+
+        if (remainingchars < 100 && remainingchars > 66) {
+            infomsg.style.color = "green";
+        }
+
+        if (remainingchars <= 66 && remainingchars > 33) {
+            infomsg.style.color = "orange";
+        }
+
+        if (remainingchars <= 33 && remainingchars >= 0) {
+            infomsg.style.color = "red";
+        }
+
+    });
+
+});
